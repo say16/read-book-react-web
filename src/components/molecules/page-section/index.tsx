985df2d6d982225c82b9de4dcf0ca76926ba755a
Ömer <omersay6@gmail.com>
2024-12-05 13:22:31 +0400

    [FIX] canvasBackground

--- src/components/molecules/page-section/index.tsx
+++ src/components/molecules/page-section/index.tsx
@@ -4,10 +4,12 @@ import { selectFileObjectUrl, selectPage, setNumPages } from '@/store/slices/pdf
 import { Card } from '@/components/ui/card'
 import { IconFileTypePdf } from '@tabler/icons-react'
 import useMarkClick from '@/utils/mark/useMarkClick'
+import { useTheme } from '@/utils/theme-provider'
 
 function PageSection() {
-  const { customTextRenderer } = useMarkClick()
   const dispatch = useDispatch()
+  const { customTextRenderer } = useMarkClick()
+  const { theme } = useTheme()
   const page = useSelector(selectPage)
   const fileObjectUrl = useSelector(selectFileObjectUrl)
 
@@ -19,7 +21,11 @@ function PageSection() {
     <Card className='flex size-fit min-h-[20rem] min-w-[20rem] items-center justify-center p-2'>
       {fileObjectUrl ? (
         <Document file={fileObjectUrl} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={page} customTextRenderer={customTextRenderer} />
+          <Page
+            pageNumber={page}
+            customTextRenderer={customTextRenderer}
+            canvasBackground={theme === 'light' ? 'white' : '#060818'}
+          />
         </Document>
       ) : (
         <div className='flex flex-col items-center justify-center gap-2 text-xl text-muted-foreground'>
